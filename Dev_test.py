93812e71f60dc6ae7534924481c1cdd8155b7f4d
RamHunuma <kdevaraj4394@gmail.com>
2025-04-06 14:45:58 +0530

    Revert "test_4"
    This reverts commit 8c187c853d7f6621af3e2bf26b5df611892e2792.

--- Dev_test.py
+++
@@ -1,8 +0,0 @@
-hungry = input("Are you hungery tyoe 'yes' or 'no': ")
-
-if hungry == 'yes':
-    print('Eat Pizza')
-    print("Eat Zibrish")
-    print("Sameline")
-else:
-    print("Do your homework")
