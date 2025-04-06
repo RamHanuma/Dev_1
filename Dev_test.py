8c187c853d7f6621af3e2bf26b5df611892e2792
RamHunuma <kdevaraj4394@gmail.com>
2025-04-06 14:05:29 +0530

    test_4

---
+++ Dev_test.py
@@ -0,0 +1,8 @@
+hungry = input("Are you hungery tyoe 'yes' or 'no': ")
+
+if hungry == 'yes':
+    print('Eat Pizza')
+    print("Eat Zibrish")
+    print("Sameline")
+else:
+    print("Do your homework")
